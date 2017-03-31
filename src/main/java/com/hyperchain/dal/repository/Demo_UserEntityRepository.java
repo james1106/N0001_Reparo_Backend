4c6c1ed83824776bd5252699033cc049e6680124
chenxiaoyang <chenxiaoyang@hyperchain.cn>
2017-03-31 10:08:23 +0800

    更新pom等配置文件

---
+++ src/main/java/com/hyperchain/dal/repository/Demo_UserEntityRepository.java
@@ -0,0 +1,7 @@
+package com.hyperchain.dal.repository;
+
+/**
+ * Created by chenxiaoyang on 2017/3/30.
+ */
+public class Demo_UserEntityRepository {
+}
---
+++ src/test/java/com/hyperchain/dal/testDemo_UserEntity.java
@@ -0,0 +1,7 @@
+package com.hyperchain.dal;
+
+/**
+ * Created by chenxiaoyang on 2017/3/30.
+ */
+public class testDemo_UserEntity {
+}
