6491ef2cc0bb92ad14b760cacf57c2744826706c
陈宇峰 <yufengcode@gmail.com>
2017-03-31 14:07:46 +0800

    测试两个合约方法

--- src/test/java/com/hyperchain/contract/CallContractTest.java
+++ src/test/java/com/hyperchain/contract/CallContractTest.java
@@ -1,5 +1,6 @@
 package com.hyperchain.contract;
 
+import cn.hyperchain.sdk.rpc.Transaction.Transaction;
 import cn.hyperchain.sdk.rpc.returns.CompileReturn;
 import com.hyperchain.ESDKConnection;
 import com.hyperchain.ESDKUtil;
@@ -39,5 +40,48 @@ public class CallContractTest {
         String contractAddress = ESDKConnection.deployContract(keyInfos.get(0), keyInfos.get(1), "123");
         Assert.assertNotNull(contractAddress);
         System.out.println("contractAddress: " + contractAddress);
+    }
+
+    @Test
+    public void invokeAddUser() throws Exception {
+
+        List<String> keyInfos = ESDKUtil.newAccount();
+        String publicKey = keyInfos.get(0);
+        String privateKey = keyInfos.get(1);
+
+        String funcName = "addUser";
+        Object[] params = new Object[4];
+        params[0] = 2;
+        params[1] = "chenyufeng2";
+        params[2] = "123456";
+        params[3] = "110";
+        Transaction transaction = ESDKUtil.getTxHash(publicKey, funcName, params);
+        transaction.sign(privateKey, null);
+
+        String result = ESDKConnection.invokeContractMethod(transaction);
+        Assert.assertNotNull(result);
+        System.out.println("invoke result: " + result);
+    }
+
+    @Test
+    public void invokeQueryUser() throws Exception {
+
+        List<String> keyInfos = ESDKUtil.newAccount();
+        String publicKey = keyInfos.get(0);
+        String privateKey = keyInfos.get(1);
+
+        String funcName = "queryUser";
+        Object[] params = new Object[1];
+        params[0] = 2;
+        Transaction transaction = ESDKUtil.getTxHash(publicKey, funcName, params);
+        transaction.sign(privateKey, null);
+
+        String result = ESDKConnection.invokeContractMethod(transaction);
+        Assert.assertNotNull(result);
+        System.out.println("invoke result: " + result);
+
+        //返回值解码
+        List<Object> retDecode = ESDKUtil.retDecode(funcName, result);
+        System.out.println("after decode result:" + retDecode);
     }
 }
