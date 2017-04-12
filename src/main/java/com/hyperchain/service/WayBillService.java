f8c021e239d8f1f11296a5650fb17fe042fdbbe5
l d y <ldy@ldeMacBook-Air.local>
2017-04-12 15:00:36 +0800

    update esdk-3.0.jar

--- src/main/java/com/hyperchain/service/WayBillService.java
+++ src/main/java/com/hyperchain/service/WayBillService.java
@@ -1,8 +1,34 @@
 package com.hyperchain.service;
 
+import com.hyperchain.controller.vo.BaseResult;
+import com.wordnik.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by ldy on 2017/4/12.
  */
 public interface WayBillService {
+
+    BaseResult<Object> generateUnConfirmedWaybill(String orderNo,
+                                                  String logisticsEnterpriseName,
+                                                  String senderEnterpriseName,
+                                                  String receiverEnterpriseName,
+                                                  int productName,
+                                                  String productQuantity,
+                                                  long productValue,
+                                                  String senderRepoEnterpriseName,
+                                                  String senderRepoCertNo,
+                                                  String receiverRepoEnterpriseName,
+                                                  String receiverRepoBusinessNo,
+                                                  HttpServletRequest request);
+
+    BaseResult<Object> generateConfirmedWaybill(String orderNo, HttpServletRequest request);
+
+    BaseResult<Object> getAllRelatedWayBillDetail(HttpServletRequest request);
+
+    BaseResult<Object> updateWayBillStatusToReceived(String orderNo, HttpServletRequest request);
 
 }
