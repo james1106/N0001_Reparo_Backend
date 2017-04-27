e15ce6d734970b752d93107dec9b2df901017785
ldy <lidongying@hyperchain.cn>
2017-04-27 15:00:20 +0800

    modify AuthFilter

--- src/main/java/com/hyperchain/common/filter/AuthFilter.java
+++ src/main/java/com/hyperchain/common/filter/AuthFilter.java
@@ -40,8 +40,8 @@ public class AuthFilter implements javax.servlet.Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
 
-        //不对swagger目录下的所有页面、登录接口、登录页面拦截
-        if (httpServletRequest.getRequestURL().indexOf("docs") > 0 || isNoFilterUri(httpServletRequest)) {
+        //不拦截的页面
+        if (isNoFilterUri(httpServletRequest)) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
@@ -83,12 +83,10 @@ public class AuthFilter implements javax.servlet.Filter {
                             LogUtil.info("token验证通过");
                             //重设cookie中的token时间戳
                             String newToken = TokenUtil.generateToken(address, roleCode);
-//                            Cookie newCookie = new Cookie("token", newToken); //错误做法，更新cookie时不能每次都new Cookie（会生成多个同名cookie），只能找到cookie并修改
                             LogUtil.info("新的token：" + newToken);
                             cookie.setValue(newToken);
                             cookie.setPath("/");
                             httpServletResponse.addCookie(cookie);
-//                            httpServletResponse.addCookie(newCookie);
                             filterChain.doFilter(servletRequest, servletResponse);
                             return;
                         }
@@ -113,19 +111,22 @@ public class AuthFilter implements javax.servlet.Filter {
     //TODO 确定登录页面url
     private void redirectToLogin(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
         String host = httpServletRequest.getScheme() + "://" + httpServletRequest.getServerName() + ":" + httpServletRequest.getServerPort();
-        httpServletResponse.sendRedirect(host + "/module/login.html");
-//        httpServletResponse.sendRedirect("http://localhost:8080/reparo/docs/index.html");
+        httpServletResponse.sendRedirect(host + "/reparo/module/login.html");
     }
 
     //不需要过滤的url
     private boolean isNoFilterUri(HttpServletRequest httpServletRequest) {
         String uri = httpServletRequest.getRequestURI().toString();
-        switch (uri) {
+        if (uri.indexOf("static") > 0 || uri.indexOf("module") > 0 || uri.indexOf("commons") > 0 || uri.indexOf("index") > 0) { //前端页面资源
+            return true;
+        }
+        if (uri.indexOf("docs") > 0) { //swagger
+            return true;
+        }
+        switch (uri) { //后台接口
             case "/reparo/v1/account/user":
                 return true;
             case "/reparo/v1/account/login":
-                return true;
-            case "/module/login.html":
                 return true;
             default:
                 return false;
