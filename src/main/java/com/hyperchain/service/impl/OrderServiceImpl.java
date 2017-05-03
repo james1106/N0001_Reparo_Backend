374bf422187e4564f330d38d398a6db6d8d54830
zhanggejin <zhanggejin@hyperchain.cn>
2017-05-03 10:12:09 +0800

    修改价格显示的错误。

--- src/main/java/com/hyperchain/service/impl/OrderServiceImpl.java
+++ src/main/java/com/hyperchain/service/impl/OrderServiceImpl.java
@@ -121,9 +121,9 @@ public class OrderServiceImpl implements OrderService{
         String payerRepoCertNo = partParams1.get(7);    //买家仓单编号
         String payeeRepoCertNo = partParams1.get(8);    //卖家仓单编号
 
-        long productUnitPrice = Long.parseLong(partParams2.get(0))/100;
+        long productUnitPrice = Long.parseLong(partParams2.get(0));
         long productQuantity = Long.parseLong(partParams2.get(1));
-        long productTotalPrice = Long.parseLong(partParams2.get(2))/100;
+        long productTotalPrice = Long.parseLong(partParams2.get(2));
         long orderGenerateTime = Long.parseLong(partParams2.get(3));
         long orderComfirmTime = partParams2.get(4).equals("") ? 0:Long.parseLong(partParams2.get(4));
         int payerRepoBusiState = partParams2.get(5).equals("") ? 0: Integer.parseInt(partParams2.get(5));
@@ -269,10 +269,10 @@ public class OrderServiceImpl implements OrderService{
             orderOverVo.setPayeeRepoName(payeeRepoName);
 
             orderOverVo.setProductQuantity(Long.parseLong(partList3.get(i*5)));
-            orderOverVo.setProductUnitPrice(ReparoUtil.convertCentToYuan(Long.parseLong(partList3.get(i*5+1))/100));
+            orderOverVo.setProductUnitPrice(ReparoUtil.convertCentToYuan(Long.parseLong(partList3.get(i*5+1))));
 
             long orderConfirmTime = partList3.get(i*5+4).equals("")? 0 : Long.parseLong(partList3.get(i*5+4));
-            orderOverVo.setProductTotalPrice(ReparoUtil.convertCentToYuan(Long.parseLong(partList3.get(i*5+2))/100));
+            orderOverVo.setProductTotalPrice(ReparoUtil.convertCentToYuan(Long.parseLong(partList3.get(i*5+2))));
             orderOverVo.setOrderGenerateTime(Long.parseLong(partList3.get(i*5+3)));
             orderOverVo.setOrderConfirmTime(orderConfirmTime);
 
