6a4d3e21b0b8909431074b5247517b2e9f7eb13e
zhanggejin <zhanggejin@hyperchain.cn>
2017-05-11 13:16:11 +0800

    修改代码格式

--- src/main/java/com/hyperchain/service/OrderService.java
+++ src/main/java/com/hyperchain/service/OrderService.java
@@ -1,5 +1,8 @@
 package com.hyperchain.service;
 
+import com.hyperchain.common.exception.ContractInvokeFailException;
+import com.hyperchain.common.exception.PasswordIllegalParam;
+import com.hyperchain.common.exception.ValueNullException;
 import com.hyperchain.contract.ContractKey;
 import com.hyperchain.controller.vo.BaseResult;
 
@@ -7,9 +10,9 @@ import com.hyperchain.controller.vo.BaseResult;
  * Created by liangyue on 2017/4/9.
  */
 public interface OrderService {
-    BaseResult<Object> createOrder(ContractKey contractKey, Object[] contractParams, String orderNo);
-    BaseResult<Object> queryOrderDetail(ContractKey contractKey, Object[] contractParams);
-    BaseResult<Object> queryAllOrderOverViewInfoList(ContractKey contractKey, Object[] contractParams);
-    BaseResult<Object> confirmOrder(ContractKey contractKey, Object[] contractParams);
+    BaseResult<Object> createOrder(ContractKey contractKey, Object[] contractParams, String orderNo) throws ContractInvokeFailException, ValueNullException, PasswordIllegalParam;
+    BaseResult<Object> queryOrderDetail(ContractKey contractKey, Object[] contractParams) throws ContractInvokeFailException, ValueNullException, PasswordIllegalParam;
+    BaseResult<Object> queryAllOrderOverViewInfoList(ContractKey contractKey, Object[] contractParams) throws ContractInvokeFailException, ValueNullException, PasswordIllegalParam;
+    BaseResult<Object> confirmOrder(ContractKey contractKey, Object[] contractParams) throws ContractInvokeFailException, ValueNullException, PasswordIllegalParam;
 
 }
--- src/main/resources/contractSourceCode/reparo.sol
+++ src/main/resources/contractSourceCode/reparo.sol
@@ -2326,21 +2326,21 @@ contract RepositoryContract{
     }
 }
 contract OrderContract{
-address owner;
-//==========运单状态=======
-uint UNDEFINED = 0;           //未定义
-uint WAITSEND = 1;            //待发货
-uint REQUESTING = 2;          //发货待响应
-uint REJECTED = 3;            //发货被拒绝
-uint SENDING = 4;             //已发货
-uint RECEIVED = 5;            //已送达
-//=========仓储状态=======
-uint WATING_INCOME_RESPONSE = 1;    //入库待响应
-uint WATING_INCOME = 2;             //待入库
-uint INCOMED = 3;                   //已入库
-uint WATING_OUTCOME_RESPONSE = 4;   //出库待响应
-uint WATING_OUTCOME = 5;            //待出库
-uint OUTCOMED = 6;                  //已出库
+    address owner;
+    //==========运单状态=======
+    uint UNDEFINED = 0;           //未定义
+    uint WAITSEND = 1;            //待发货
+    uint REQUESTING = 2;          //发货待响应
+    uint REJECTED = 3;            //发货被拒绝
+    uint SENDING = 4;             //已发货
+    uint RECEIVED = 5;            //已送达
+    //=========仓储状态=======
+    uint WATING_INCOME_RESPONSE = 1;    //入库待响应
+    uint WATING_INCOME = 2;             //待入库
+    uint INCOMED = 3;                   //已入库
+    uint WATING_OUTCOME_RESPONSE = 4;   //出库待响应
+    uint WATING_OUTCOME = 5;            //待出库
+    uint OUTCOMED = 6;                  //已出库
 
     //=========仓单状态=======
     uint TRANSABLE = 1;                //可流转
